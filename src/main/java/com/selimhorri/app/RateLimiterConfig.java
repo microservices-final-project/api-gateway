ef3904b2376dc975bd6201332597a1e2c9725caa
alejolonber <alejandrolonber25@gmail.com>
2025-06-12 19:35:10 -0500

    refactor: throttling design pattern changed

--- src/main/java/com/selimhorri/app/RateLimiterConfig.java
+++ src/main/java/com/selimhorri/app/RateLimiterConfig.java
@@ -1,3 +1,17 @@
+package com.selimhorri.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import reactor.core.publisher.Mono;
+
+import java.net.InetSocketAddress;
+
+
 @Configuration
 public class RateLimiterConfig {
 
